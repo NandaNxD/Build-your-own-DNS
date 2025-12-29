e41d2318d5a0b652f35d192d83ff5a2b5d3c383b
Nandan T S <nanven9@gmail.com>
2025-12-29 22:33:03 +0530

    Removed lombok and added manual getter and setter to support online test runner

--- src/main/java/DNSMessage.java
+++ src/main/java/DNSMessage.java
@@ -1,12 +1,67 @@
-import lombok.AllArgsConstructor;
-import lombok.Data;
+import java.util.Objects;
 
-@Data
-@AllArgsConstructor
 public class DNSMessage {
+
     private DNSMessageHeader dnsMessageHeader;
     private DNSMessageQuestion dnsMessageQuestion;
     private DNSMessageAnswer dnsMessageAnswer;
     private DNSMessageAuthority dnsMessageAuthority;
+
+    // All-args constructor
+    public DNSMessage(
+            DNSMessageHeader dnsMessageHeader,
+            DNSMessageQuestion dnsMessageQuestion,
+            DNSMessageAnswer dnsMessageAnswer,
+            DNSMessageAuthority dnsMessageAuthority
+    ) {
+        this.dnsMessageHeader = dnsMessageHeader;
+        this.dnsMessageQuestion = dnsMessageQuestion;
+        this.dnsMessageAnswer = dnsMessageAnswer;
+        this.dnsMessageAuthority = dnsMessageAuthority;
+    }
+
+    // Getters & Setters
+    public DNSMessageHeader getDnsMessageHeader() {
+        return dnsMessageHeader;
+    }
+
+    public void setDnsMessageHeader(DNSMessageHeader dnsMessageHeader) {
+        this.dnsMessageHeader = dnsMessageHeader;
+    }
+
+    public DNSMessageQuestion getDnsMessageQuestion() {
+        return dnsMessageQuestion;
+    }
+
+    public void setDnsMessageQuestion(DNSMessageQuestion dnsMessageQuestion) {
+        this.dnsMessageQuestion = dnsMessageQuestion;
+    }
+
+    public DNSMessageAnswer getDnsMessageAnswer() {
+        return dnsMessageAnswer;
+    }
+
+    public void setDnsMessageAnswer(DNSMessageAnswer dnsMessageAnswer) {
+        this.dnsMessageAnswer = dnsMessageAnswer;
+    }
+
+    public DNSMessageAuthority getDnsMessageAuthority() {
+        return dnsMessageAuthority;
+    }
+
+    public void setDnsMessageAuthority(DNSMessageAuthority dnsMessageAuthority) {
+        this.dnsMessageAuthority = dnsMessageAuthority;
+    }
+
+    // toString()
+    @Override
+    public String toString() {
+        return "DNSMessage{" +
+                "dnsMessageHeader=" + dnsMessageHeader +
+                ", dnsMessageQuestion=" + dnsMessageQuestion +
+                ", dnsMessageAnswer=" + dnsMessageAnswer +
+                ", dnsMessageAuthority=" + dnsMessageAuthority +
+                '}';
+    }
 
 }
--- src/main/java/DNSMessageHeader.java
+++ src/main/java/DNSMessageHeader.java
@@ -1,11 +1,9 @@
-import lombok.AllArgsConstructor;
-import lombok.Data;
-
 import java.nio.ByteBuffer;
+import java.util.Arrays;
+import java.util.Objects;
 
-@Data
-@AllArgsConstructor
 public class DNSMessageHeader {
+
     /**
      * DNS Message header is always 12 bytes long
      */
@@ -14,7 +12,7 @@ public class DNSMessageHeader {
      * Its format **
      *
      * 16 bits: Packet Identifier ID
-     * 
+     *
      * 1 bit : Query Response QR
      * 4 bits: Operation Code OPCODE
      * 1 bit : Authoritative Answer AA
@@ -33,17 +31,109 @@ public class DNSMessageHeader {
 
     private byte[] dnsOriginalMessageHeaderInBytes;
 
-     // To get packet ID
-     private byte[] packetId;
-     private byte[] flags;
-     private byte[] questionCount;
-     private byte[] answerCount;
-     private byte[] authorityCount;
-     private byte[] additionalCount;
+    // Header fields (each 16 bits / 2 bytes)
+    private byte[] packetId;
+    private byte[] flags;
+    private byte[] questionCount;
+    private byte[] answerCount;
+    private byte[] authorityCount;
+    private byte[] additionalCount;
 
-     public int getPacketIdInInt(){
-         return Short.toUnsignedInt(ByteBuffer.wrap(getPacketId()).getShort());
-     }
+    // All-args constructor
+    public DNSMessageHeader(
+            byte[] dnsOriginalMessageHeaderInBytes,
+            byte[] packetId,
+            byte[] flags,
+            byte[] questionCount,
+            byte[] answerCount,
+            byte[] authorityCount,
+            byte[] additionalCount
+    ) {
+        this.dnsOriginalMessageHeaderInBytes = dnsOriginalMessageHeaderInBytes;
+        this.packetId = packetId;
+        this.flags = flags;
+        this.questionCount = questionCount;
+        this.answerCount = answerCount;
+        this.authorityCount = authorityCount;
+        this.additionalCount = additionalCount;
+    }
 
-    
+    public int getPacketIdInInt() {
+        return Short.toUnsignedInt(
+                ByteBuffer.wrap(getPacketId()).getShort()
+        );
+    }
+
+    // Getters & Setters
+    public byte[] getDnsOriginalMessageHeaderInBytes() {
+        return dnsOriginalMessageHeaderInBytes;
+    }
+
+    public void setDnsOriginalMessageHeaderInBytes(byte[] dnsOriginalMessageHeaderInBytes) {
+        this.dnsOriginalMessageHeaderInBytes = dnsOriginalMessageHeaderInBytes;
+    }
+
+    public byte[] getPacketId() {
+        return packetId;
+    }
+
+    public void setPacketId(byte[] packetId) {
+        this.packetId = packetId;
+    }
+
+    public byte[] getFlags() {
+        return flags;
+    }
+
+    public void setFlags(byte[] flags) {
+        this.flags = flags;
+    }
+
+    public byte[] getQuestionCount() {
+        return questionCount;
+    }
+
+    public void setQuestionCount(byte[] questionCount) {
+        this.questionCount = questionCount;
+    }
+
+    public byte[] getAnswerCount() {
+        return answerCount;
+    }
+
+    public void setAnswerCount(byte[] answerCount) {
+        this.answerCount = answerCount;
+    }
+
+    public byte[] getAuthorityCount() {
+        return authorityCount;
+    }
+
+    public void setAuthorityCount(byte[] authorityCount) {
+        this.authorityCount = authorityCount;
+    }
+
+    public byte[] getAdditionalCount() {
+        return additionalCount;
+    }
+
+    public void setAdditionalCount(byte[] additionalCount) {
+        this.additionalCount = additionalCount;
+    }
+
+
+    // toString()
+    @Override
+    public String toString() {
+        return "DNSMessageHeader{" +
+                "dnsOriginalMessageHeaderInBytes=" + Arrays.toString(dnsOriginalMessageHeaderInBytes) +
+                ", packetId=" + Arrays.toString(packetId) +
+                ", flags=" + Arrays.toString(flags) +
+                ", questionCount=" + Arrays.toString(questionCount) +
+                ", answerCount=" + Arrays.toString(answerCount) +
+                ", authorityCount=" + Arrays.toString(authorityCount) +
+                ", additionalCount=" + Arrays.toString(additionalCount) +
+                '}';
+    }
+
 }
