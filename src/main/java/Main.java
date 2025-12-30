7d1489e7c70b44c083fd5c8225c936247ee0db42
Nandan T S <nanven9@gmail.com>
2025-12-30 18:30:33 +0530

    Added support for header section response

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -18,9 +18,17 @@ public class Main {
                 DNSMessageHeader dnsMessageHeader=dnsRequestMessage.getDnsMessageHeader();
                 DNSMessageQuestion dnsMessageQuestion=dnsRequestMessage.getDnsMessageQuestion();
 
-                dnsMessageHeader.setFlags(new byte[]{-128,0});
+
+                //Flags:  CODE BITS -> QR 1 | OPCODE 4 | AA 1 | TC 1 | RD 1 | RA 1 | Z 3 | RCODE 4
+
+                byte opCode= (byte) (dnsMessageHeader.getFlags()[0] & Byte.parseByte("01111000",2));
+
+                byte[] flags=new byte[]{(byte)(-128 | opCode | (dnsMessageHeader.getFlags()[0] & 1)),(byte)(opCode==0?0:4)};
+
+                dnsMessageHeader.setFlags(flags);
                 dnsMessageHeader.setQuestionCount(new byte[]{0,1});
                 dnsMessageHeader.setAnswerCount(new byte[]{0,1});
+
 
                 DNSMessageAnswer dnsMessageAnswer= DNSMessageParser.getDNSMessageAnswer(dnsMessageQuestion);
 
